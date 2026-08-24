3b4e6e43864f1f130c4ca6574cddf078062c1239
bhxwya <bhawya18207@gmail.com>
2026-08-24 14:00:29 +0530

    Set up organizer class and CLI

---
+++ main.py
@@ -0,0 +1,13 @@
+from organizer import FileOrganizer
+
+
+def main():
+    folder_path = input("Enter the folder path: ")
+
+    organizer = FileOrganizer(folder_path)
+
+    print(f"Folder selected: {organizer.folder_path}")
+
+
+if __name__ == "__main__":
+    main()
--- organizer.py
+++ organizer.py
@@ -1,6 +1,9 @@
-def main():
-    pass
+from pathlib import Path
 
 
-if __name__ == "__main__":
-    main()
+class FileOrganizer:
+    def __init__(self, folder_path):
+        self.folder_path = Path(folder_path)
+        
+    def organize(self):
+        pass
