da850503fc4de3be7cdb9c26c83b68142054b7e4
bhxwya <bhawya18207@gmail.com>
2026-08-24 16:56:28 +0530

    Add file discovery to organizer

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def main():
     folder_path = input("Enter the folder path: ")
 
     organizer = FileOrganizer(folder_path)
-
     print(f"Folder selected: {organizer.folder_path}")
 
+    organizer.organize()
 
 if __name__ == "__main__":
     main()
--- organizer.py
+++ organizer.py
@@ -4,6 +4,8 @@ from pathlib import Path
 class FileOrganizer:
     def __init__(self, folder_path):
         self.folder_path = Path(folder_path)
-        
+
     def organize(self):
-        pass
+        for item in self.folder_path.iterdir():
+            if item.is_file():
+                print(item)
