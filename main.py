ce1a2e199b35318b39b19bff45f385adc04f6b47
bhxwya <bhawya18207@gmail.com>
2026-08-25 12:26:00 +0530

    Add folder validation and error handling

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from organizer import FileOrganizer
 
 def main():
     folder_path = input("Enter the folder path: ")
-
-    organizer = FileOrganizer(folder_path)
+    
+    try:
+        organizer = FileOrganizer(folder_path)
+    except ValueError as e:
+        print(f"Error: {e}")
+        return
+    
     print(f"Folder selected: {organizer.folder_path}")
-
     organizer.organize()
 
 if __name__ == "__main__":
--- organizer.py
+++ organizer.py
@@ -5,6 +5,10 @@ from categories import CATEGORIES
 class FileOrganizer:
     def __init__(self, folder_path):
         self.folder_path = Path(folder_path)
+        
+        if not self.folder_path.exists() or not self.folder_path.is_dir():
+            raise ValueError("Invalid folder path.")
+          
 
     def get_category(self, item):
         for category, extensions in CATEGORIES.items():
@@ -13,12 +17,31 @@ class FileOrganizer:
         return "Others"
 
     def organize(self):
+        count = 0
+
         for item in self.folder_path.iterdir():
             if item.is_file():
                 category = self.get_category(item)
                 destination = self.folder_path / category
                 destination.mkdir(exist_ok=True)
+
                 put_files = destination / item.name
-                item.rename(put_files)
-                
+
+                try:
+                    item.rename(put_files)
+
+                except FileExistsError:
+                    number = 1
+
+                    while put_files.exists():
+                        changed_name = item.stem + "_" + \
+                            str(number) + item.suffix
+                        put_files = destination / changed_name
+                        number += 1
+
+                    item.rename(put_files)
+
+                count += 1
+
         print("Files organized successfully!")
+        print(f"{count} files organized.")
